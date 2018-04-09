44d37eb6a62cac9b50fbc5cb5c0e8e48ba88c28c
muse90673 <38189037+muse90673@users.noreply.github.com>
2018-04-09 23:39:22 +0800

    Add files via upload

---
+++ auto-exec.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yum -y install wget
+wget -N --no-check-certificate https://raw.githubusercontent.com/wn789/serverspeeder/master/serverspeeder.sh
+bash serverspeeder.sh
+cd shad*/shad*
+python server.py -p 2333 -k chao -m aes-256-cfb -O auth_sha1_v4 -o tls1.2_ticket_auth -d start
+echo "ssr部署完成，已自动打开日志文件："
+tail -f /var/log/shadowsocksr.log
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#设置.bash_profile文件
+
+#安装组件
+yum install git -y
+git clone -b manyuser https://github.com/shadowsocksr-backup/shadowsocksr.git
+cd shad*/shad*
+python server.py -p 2333 -k chao -m aes-256-cfb -O auth_sha1_v4 -o tls1.2_ticket_auth -d start
+/sbin/iptables -I INPUT -p tcp --dport 2333 -j ACCEPT
+/etc/rc.d/init.d/iptables save
+/etc/rc.d/init.d/iptables restart
+rpm -ivh http://xz.wn789.com/CentOSkernel/kernel-firmware-2.6.32-504.3.3.el6.noarch.rpm
+rpm -ivh http://xz.wn789.com/CentOSkernel/kernel-2.6.32-504.3.3.el6.x86_64.rpm --force
+reboot
